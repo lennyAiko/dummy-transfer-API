2a5ab253259c11a5ab62fa906da2884893d5a232
Barbara Odozi <barbs.nene@gmail.com>
2023-06-28 13:51:10 +0100

    add user registration endpoint

---
+++ api/models/User.js
@@ -0,0 +1,35 @@
+/**
+ * User.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  attributes: {
+    uuid: {
+      type: "string",
+      unique: true,
+      required: true,
+    },
+    email: {
+      type: "string",
+      unique: true,
+      required: true,
+    },
+    name: {
+      type: "string",
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: "string",
+      required: true,
+      minLength: 8,
+    },
+    role: {
+      type: "string",
+      defaultsTo: "user",
+    },
+  },
+};
